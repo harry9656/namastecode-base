eab3689710e469cb3938887f2056dcb58d0ec72d
Harry9656 <singharpal9656@gmail.com>
2024-05-16 19:32:03 +0200

    Created Parent Repository

---
+++ src/test/java/com/namastecode/parent/ParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.namastecode.parent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
