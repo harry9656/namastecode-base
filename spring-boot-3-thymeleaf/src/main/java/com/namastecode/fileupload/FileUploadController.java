d6dc568e6186e98c99b3cb971f090af6449e8b11
Harpal Singh <singharpal9656@gmail.com>
2025-04-13 14:41:10 +0530

    Fix

--- spring-boot-3-thymeleaf/src/main/java/com/namastecode/fileupload/FileUploadController.java
+++ spring-boot-3-thymeleaf/src/main/java/com/namastecode/fileupload/FileUploadController.java
@@ -2,6 +2,7 @@ package com.namastecode.fileupload;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
