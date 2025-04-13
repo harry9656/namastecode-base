16fa3da0209c8ba12df7c999218d16f3db0e7481
Harpal Singh <singharpal9656@gmail.com>
2025-04-13 14:32:51 +0530

    Disable test

--- spring-boot-3/src/test/java/com/namastecode/restclient/OpenAIRestClientTest.java
+++ spring-boot-3/src/test/java/com/namastecode/restclient/OpenAIRestClientTest.java
@@ -1,9 +1,11 @@
 package com.namastecode.restclient;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@Disabled
 class OpenAIRestClientTest {
 
     @Test
